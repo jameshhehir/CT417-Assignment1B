e72aea97953d05b89c93c9c9e7029f9e49e43bb9
James Hehir <j.hehir1@nuigalway.ie>
2018-10-24 20:51:30 +0100

    fixed pom and finished java file

--- src/main/java/jimmyhehir/TestStudents.java
+++ src/main/java/jimmyhehir/TestStudents.java
@@ -6,39 +6,38 @@ public class TestStudents
 {
 
     public static void main( String[] args ) {
-        Module EE443 = new Module("BE Project","EE443"); //ECE, EE
-        Module CT417 = new Module("Software Engineering III", "CT417"); //ECE, CS
-        Module CT475 = new Module("Machine Learning & Data Mining", "CT475"); //ECE, CS
-        Module EE451 = new Module("System on Chip Design", "EE451"); //ECE, EE
+        Module EE443 = new Module("BE Project","EE443"); 
+        Module CT417 = new Module("Software Engineering III", "CT417"); 
+        Module CT475 = new Module("Machine Learning & Data Mining", "CT475");
+        Module EE451 = new Module("System on Chip Design", "EE451"); 
         
         Course CS = new Course("Computer Science"); //1, 7
         Course ECE = new Course("Electronic & Computer Engineering"); // 3, 4, 5, 6, 8, 9
         Course EEE = new Course("Electrical and Electronic Engineering"); //0, 2
         
     	ArrayList<Student> allstudents = new ArrayList<Student>();
-        allstudents = createlistofStudents(allstudents); //creates a list of students to use for modules
-        //populate modules with students
-        EE443.addStudent(allstudents.get(0)); EE443.addStudent(allstudents.get(2)); //EE Students
-        EE443.addStudent(allstudents.get(3)); EE443.addStudent(allstudents.get(4)); //ECE Students
-        EE443.addStudent(allstudents.get(5)); EE443.addStudent(allstudents.get(6)); //..
-        EE443.addStudent(allstudents.get(8)); EE443.addStudent(allstudents.get(9)); //ECE Students
+        allstudents = createlistofStudents(allstudents); 
+        EE443.addStudent(allstudents.get(0)); EE443.addStudent(allstudents.get(2)); 
+        EE443.addStudent(allstudents.get(3)); EE443.addStudent(allstudents.get(4)); 
+        EE443.addStudent(allstudents.get(5)); EE443.addStudent(allstudents.get(6)); 
+        EE443.addStudent(allstudents.get(8)); EE443.addStudent(allstudents.get(9)); 
         
-        EE451.addStudent(allstudents.get(0)); EE451.addStudent(allstudents.get(2)); //EE Students
-        EE451.addStudent(allstudents.get(3)); EE451.addStudent(allstudents.get(4)); //ECE Students
-        EE451.addStudent(allstudents.get(5)); EE451.addStudent(allstudents.get(6)); //..
-        EE451.addStudent(allstudents.get(8)); EE451.addStudent(allstudents.get(9)); //ECE Students
+        EE451.addStudent(allstudents.get(0)); EE451.addStudent(allstudents.get(2)); 
+        EE451.addStudent(allstudents.get(3)); EE451.addStudent(allstudents.get(4)); 
+        EE451.addStudent(allstudents.get(5)); EE451.addStudent(allstudents.get(6)); 
+        EE451.addStudent(allstudents.get(8)); EE451.addStudent(allstudents.get(9)); 
         
-        CT417.addStudent(allstudents.get(3)); CT417.addStudent(allstudents.get(4)); //ECE Students
-        CT417.addStudent(allstudents.get(5)); CT417.addStudent(allstudents.get(6)); //..
-        CT417.addStudent(allstudents.get(8)); CT417.addStudent(allstudents.get(9)); //ECE Students
-        CT417.addStudent(allstudents.get(1)); CT417.addStudent(allstudents.get(7)); //CS Students
+        CT417.addStudent(allstudents.get(3)); CT417.addStudent(allstudents.get(4)); 
+        CT417.addStudent(allstudents.get(5)); CT417.addStudent(allstudents.get(6)); 
+        CT417.addStudent(allstudents.get(8)); CT417.addStudent(allstudents.get(9)); 
+        CT417.addStudent(allstudents.get(1)); CT417.addStudent(allstudents.get(7)); 
         
-        CT475.addStudent(allstudents.get(3)); CT475.addStudent(allstudents.get(4)); //ECE Students
-        CT475.addStudent(allstudents.get(5)); CT475.addStudent(allstudents.get(6)); //..
-        CT475.addStudent(allstudents.get(8)); CT475.addStudent(allstudents.get(9)); //ECE Students
-        CT475.addStudent(allstudents.get(1)); CT475.addStudent(allstudents.get(7)); //CS Students
+        CT475.addStudent(allstudents.get(3)); CT475.addStudent(allstudents.get(4)); 
+        CT475.addStudent(allstudents.get(5)); CT475.addStudent(allstudents.get(6)); 
+        CT475.addStudent(allstudents.get(8)); CT475.addStudent(allstudents.get(9)); 
+        CT475.addStudent(allstudents.get(1)); CT475.addStudent(allstudents.get(7));
 
-        CS.addModule(CT475); CS.addModule(CT417); //add modules to courses
+        CS.addModule(CT475); CS.addModule(CT417); 
         ECE.addModule(EE451); ECE.addModule(CT475); ECE.addModule(CT417); ECE.addModule(EE443);
         EEE.addModule(EE451); EEE.addModule(EE443);
     	
@@ -47,21 +46,19 @@ public class TestStudents
     	
         System.out.println("List of students: \n");
         
-        ArrayList<Student> checkedStudents = new ArrayList<Student>(); //list of students who have already been printed to console 
+        ArrayList<Student> checkedStudents = new ArrayList<Student>(); 
         
-    	for(Course course : allCourses) { //iterate through each course
-    		for(Module module : course.listModules()) { //iterate through each module in each course
-    			for(Student student : module.listStudents()) { //iterate through each student in each course
-        			if(!checkedStudents.contains(student)) { //if student already done, skip if they appear again in other modules
-        				checkedStudents.add(student); //adds student to list so can be skippedi n future
-    				ArrayList<Module> listofmods  = new ArrayList<Module>(); //list of modules that student takes
+    	for(Course course : allCourses) { 
+    		for(Module module : course.listModules()) { 
+    			for(Student student : module.listStudents()) { 
+        			if(!checkedStudents.contains(student)) { 
+        				checkedStudents.add(student); 
+    				ArrayList<Module> listofmods  = new ArrayList<Module>(); 
     				for(Course cc : allCourses) {
     					for(Module mod : cc.listModules()){
-    						//if mod not already present in listofmods and student is in mod, add to student's list of mods
     						if((mod.listStudents().contains(student)) && !(listofmods.contains(mod))) listofmods.add(mod);
     					}
     				}
-    				//Prints out student username and course name depending on which modules student has
     				if( listofmods.contains(EE443) && listofmods.contains(CT475) ) 
     					System.out.println(student.getUsername() + ": " + ECE.getName());
     				if( listofmods.contains(EE443) && !listofmods.contains(CT475) ) 
@@ -69,7 +66,7 @@ public class TestStudents
     				if( listofmods.contains(CT475) && !listofmods.contains(EE443) ) 
     					System.out.println(student.getUsername() + ": " + CS.getName());
     				for(Module m : listofmods) {
-    					System.out.print(m.getName() + ". "); //prints student's modules
+    					System.out.print(m.getName() + ". "); 
     					}
     				System.out.println("");
     				}
@@ -77,21 +74,15 @@ public class TestStudents
     			}
     		}
     }
-    
-    /**
-     * Fills allstudents with 10 students
-     */
+
 	private static ArrayList<Student> createlistofStudents(ArrayList<Student> list) {
-		list.add(new Student("Jack Smith", 1, 02, 1994));
-		list.add(new Student("Eamon Dunphy", 12, 06, 1993));
-		list.add(new Student("Jeff Hendrick", 5, 11, 1991));
-		list.add(new Student("Wes Hoolahan", 13, 9, 1992));
-		list.add(new Student("Seamus Coleman", 1, 12, 1994));
-		list.add(new Student("Gareth Bale", 4, 9, 1990));
-		list.add(new Student("William Hill", 14, 05, 1995));
-		list.add(new Student("James Byrne", 19, 8, 1998));
-		list.add(new Student("Alice Coleman", 21, 03, 1993));
-		list.add(new Student("Shauna Bourke", 19, 11, 1994));		
+		list.add(new Student("Jimmy Hehir", 13, 9, 1995));
+		list.add(new Student("Mac Macfold", 18, 06, 1997));
+		list.add(new Student("Brendan Dunne", 5, 11, 1998));
+		list.add(new Student("Jeff Dunham", 13, 9, 1998));
+		list.add(new Student("John Corridon", 1, 12, 1996));
+		list.add(new Student("Jack Barry", 4, 9, 1997));
+		list.add(new Student("John Doe", 14, 05, 1994));	
 		
 		return list;
 	}
